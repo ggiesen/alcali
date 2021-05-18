219d6255bbbff03287f4219524a88fdd7ecb63f5
Gary T. Giesen <ggiesen@users.noreply.github.com>
2021-05-18 19:12:07 -0400

    Tighten up security on eval()

--- config/ldap_config.py
+++ config/ldap_config.py
@@ -2,6 +2,14 @@ import os
 import ldap
 from django_auth_ldap.config import LDAPGroupQuery, LDAPSearch, GroupOfNamesType
 
+# List of permitted locals for eval()
+EVAL_LOCALS = {
+    "ldap": ldap,
+    "LDAPGroupQuery": LDAPGroupQuery,
+    "LDAPSearch": LDAPSearch,
+    "GroupOfNamesType": GroupOfNamesType,
+}
+
 # Baseline configuration.
 AUTH_LDAP_SERVER_URI = os.environ.get("AUTH_LDAP_SERVER_URI", "ldap://localhost")
 
@@ -21,19 +29,21 @@ else:
 # If your configuration makes any references to LDAP groups, this and
 # AUTH_LDAP_GROUP_TYPE must be set.
 if os.environ.get("AUTH_LDAP_GROUP_SEARCH"):
-    AUTH_LDAP_GROUP_SEARCH = eval(os.environ.get("AUTH_LDAP_GROUP_SEARCH"))
+    AUTH_LDAP_GROUP_SEARCH = eval(os.environ.get("AUTH_LDAP_GROUP_SEARCH"), {'__builtins__': None}, EVAL_LOCALS)
 
 # An LDAPGroupType instance describing the type of group returned by AUTH_LDAP_GROUP_SEARCH.
 if os.environ.get("AUTH_LDAP_GROUP_TYPE"):
-    AUTH_LDAP_GROUP_TYPE = eval(os.environ.get("AUTH_LDAP_GROUP_TYPE"))
+    AUTH_LDAP_GROUP_TYPE = eval(os.environ.get("AUTH_LDAP_GROUP_TYPE"), {'__builtins__': None}, EVAL_LOCALS)
 
 # Simple group restrictions
-AUTH_LDAP_REQUIRE_GROUP = eval(os.environ.get("AUTH_LDAP_REQUIRE_GROUP"))
-AUTH_LDAP_DENY_GROUP = eval(os.environ.get("AUTH_LDAP_DENY_GROUP"))
+if os.environ.get("AUTH_LDAP_REQUIRE_GROUP"):
+    AUTH_LDAP_REQUIRE_GROUP = eval(os.environ.get("AUTH_LDAP_REQUIRE_GROUP"), {'__builtins__': None}, EVAL_LOCALS)
+if os.environ.get("AUTH_LDAP_DENY_GROUP"):
+    AUTH_LDAP_DENY_GROUP = eval(os.environ.get("AUTH_LDAP_DENY_GROUP"), {'__builtins__': None}, EVAL_LOCALS)
 
 # Define user flags based on group membership. Currently only "is_active" and "is_staff" are supported.
 if os.environ.get("AUTH_LDAP_USER_FLAGS_BY_GROUP"):
-    AUTH_LDAP_USER_FLAGS_BY_GROUP = eval(os.environ.get("AUTH_LDAP_USER_FLAGS_BY_GROUP"))
+    AUTH_LDAP_USER_FLAGS_BY_GROUP = eval(os.environ.get("AUTH_LDAP_USER_FLAGS_BY_GROUP"), {'__builtins__': None}, EVAL_LOCALS)
 
 # Populate the Django user from the LDAP directory.
 AUTH_LDAP_USER_ATTR_MAP = {
